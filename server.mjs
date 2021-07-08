d376125788eda3e82d4fa7d257dca5d8d699d032
LimitlessSocks <socktheringleader@gmail.com>
2021-07-08 16:11:43 -0400

    add bear to serve domain

--- server.mjs
+++ server.mjs
@@ -33,7 +33,7 @@ app.use(express.urlencoded({
     extended: true
 }));
 
-const SERVE_DOMAINS = [ "gatr", "snek", "capy", "fish" ];
+const SERVE_DOMAINS = [ "gatr", "snek", "capy", "fish", "bear" ];
 const clientDir = __dirname + "/public";
 
 const readBodyData = async function (req, res, next) {
